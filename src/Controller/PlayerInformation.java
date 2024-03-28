75ed0a376a2158295500f3d756b524fd20bf2d93
nghiaphan04 <nghiaphan04dev@gmail.com>
2024-03-28 18:59:02 +0700

    connect to db

---
+++ src/Controller/PlayerInformation.java
@@ -0,0 +1,15 @@
+package Controller;
+
+public class PlayerInformation {
+	public String ID;
+	public String NamePlayer;
+	public String Score;
+	public String Times;
+	public PlayerInformation(String ID,String NamePlayer,  String Score,String Times) {
+		this.ID = ID;
+		this.NamePlayer = NamePlayer;
+		this.Score = Score;
+		this.Times = Times;
+	}
+
+}
--- src/database/connectDB.java
+++ src/database/connectDB.java
@@ -1,24 +1,56 @@
 package database;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import Controller.PlayerInformation;
+
 public class connectDB {
-	
+    private String connectionUrl;
 
-	
-	public static void addDataToDB(int ID, String NamePlayer, int score, String times) {}
-	public static void main(String []args) {
-		String url = "jdbc:sqlserver://NGUYEN-VIET-HUNG\\SQLEXPRESS:1433;databaseName=player;trustServerCertificate=true";
-		String USERNAME = "sa";
-	    String PASSWORD = "102004";
-	    try {
-			Connection conn = DriverManager.getConnection(url, USERNAME, PASSWORD);
-			System.out.println("khe ");
-		} catch (SQLException e) {
-			System.out.println("kheeee ");
-			e.printStackTrace();
-		}
-	}
+    public connectDB() {
+        this.connectionUrl = "jdbc:sqlserver://PHANDINHNGHIA\\SQLEXPRESS02:1433;databaseName=player;user=sa;password=nghia;trustServerCertificate=true";
+    }
+
+    public void addDataToDB(int ID,String NamePlayer,  int Score,String Times ) {
+        try (Connection con = DriverManager.getConnection(this.connectionUrl);
+             Statement stmt = con.createStatement()) {
+
+            String SQL =
+ "INSERT INTO player (ID, NamePlayer, Score, Times) VALUES ("+ ID  + ", '" + NamePlayer + "', " + Score + ", '" + Times + "')";
+            stmt.executeUpdate(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<PlayerInformation> getDataFromDB() {
+    	ArrayList<PlayerInformation> arrInformation = new ArrayList<>();
+        try (Connection con = DriverManager.getConnection(this.connectionUrl);
+             Statement stmt = con.createStatement()) {
+
+            String SQL = "SELECT * FROM player";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while (rs.next()) {
+            	arrInformation.add(new PlayerInformation(rs.getString("ID"),rs.getString("NamePlayer"),rs.getString("Score"),rs.getString("Times")));
+            	
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+  
+		return arrInformation;
+    }
+
+    public static void main(String[] args) {
+    	ArrayList<PlayerInformation> arrInformation = new ArrayList<>();
+        connectDB a = new connectDB();
+        a.addDataToDB(5, "User5", 55, "2024-03-28 07:30:00");
+        arrInformation = a.getDataFromDB();
+        System.out.println(arrInformation.get(1).NamePlayer);
+    }
 }
-
